fd4c6ba6e1bf991b1766be19ca2fbbced1b6be53
Sarah Wostenberg <swostenberg@gmail.com>
2020-12-27 01:14:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+
+def fizzbuzz(int)
+  by3 = int % 3 == 0 
+  by5 = int % 5 == 0 
+  
+ if by3 && ! by5  
+ "Fizz"
+ elsif ! by3 && by5
+ "Buzz"
+ elsif by3 && by5
+  "FizzBuzz"
+  end
+end 
