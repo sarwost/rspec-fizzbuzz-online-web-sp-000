3e7e0823a56c069fee323e741a48a4115eb0a633
Sarah Wostenberg <swostenberg@gmail.com>
2020-12-27 01:02:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,11 @@
 
 
 def fizzbuzz(int)
-
- if int % 3 == 0 && int % 5 == 0  
-  "FizzBuzz"
- elsif int % 3 == 0 && int % 5 != 0 
+ if int % 3 == 0 && int % 5 != 0 
  "Fizz"
  elsif int % 5 == 0 && int % 3 != 0 
  "Buzz"
+ elsif int % 3 == 0 && int % 5 == 0  
+  "FizzBuzz"
   end
 end 
